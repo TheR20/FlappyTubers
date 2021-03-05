29ae3feabe7c90261cbfe080c012052e6e365848
TheR20 <r20_banana@hotmail.com>
2021-03-04 21:42:26 -0600

    Juego Basico agregando tu puntuacion

--- App.js
+++ App.js
@@ -1,114 +1,211 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { StyleSheet, View,TouchableOpacity,Text,Image } from 'react-native';
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
+import Bird from './src/components/Bird';
+import Wall from './src/components/Wall';
+import Constants from './src/Constants';
+import Physics from './src/Physics';
+import Floor from './src/components/Floor';
+export const randomBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export const generatePipes = () => {
+    let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+    let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    let sizes = [topPipeHeight, bottomPipeHeight]
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    if (Math.random() < 0.5) {
+        sizes = sizes.reverse();
+    }
+
+
+    return sizes;
+}
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+      running: true,
+      score: 0
+  };
+        this.gameEngine = null;
+
+        this.entities = this.setupWorld();
+    }
+
+    setupWorld = () => {
+          let engine = Matter.Engine.create({ enableSleeping: false });
+          let world = engine.world;
+          world.gravity.y = 0.0;
+
+          let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT / 2, Constants.BIRD_WIDTH, Constants.BIRD_HEIGHT);
+          //bird.restitution = 20;
+
+          let floor1 = Matter.Bodies.rectangle(
+              Constants.MAX_WIDTH / 2,
+              Constants.MAX_HEIGHT - 25,
+              Constants.MAX_WIDTH + 4,
+              50, { isStatic: true }
+          );
+          let floor2 = Matter.Bodies.rectangle(
+              Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2),
+              Constants.MAX_HEIGHT - 25,
+              Constants.MAX_WIDTH + 4,
+              50, { isStatic: true }
+          );
+
+          Matter.World.add(world, [bird, floor1]);
+          Matter.Events.on(engine, 'collisionStart', (event) => {
+              var pairs = event.pairs;
+
+              this.gameEngine.dispatch({ type: "game-over"});
+
+          });
+
+          return {
+              physics: { engine: engine, world: world },
+              floor1: { body: floor1, renderer: Floor },
+              floor2: { body: floor2, renderer: Floor },
+              bird: { body: bird, pose: 1, renderer: Bird},
+          }
+      }
+
+      onEvent = (e) => {
+           if (e.type === "game-over"){
+               //Alert.alert("Game Over");
+               this.setState({
+                   running: false
+               });
+           } else if (e.type === "score") {
+               this.setState({
+                   score: this.state.score + 1
+               })
+           }
+       }
+
+       reset = () => {
+
+           this.gameEngine.swap(this.setupWorld());
+           this.setState({
+               running: true,
+               score: 0
+           });
+       }
+
+    render() {
+        return (
+            <View style={styles.container}>
+            <Image source={Images.background} style={styles.backgroundImage} resizeMode="stretch" />
+                <GameEngine
+                    ref={(ref) => { this.gameEngine = ref; }}
+                    style={styles.gameContainer}
+                    systems={[Physics]}
+                    running={this.state.running}
+                    onEvent={this.onEvent}
+                    entities={this.entities}>
+
+                </GameEngine>
+                <Text style={styles.score}>{this.state.score}</Text>
+                 {!this.state.running && <TouchableOpacity style={styles.fullScreenButton} onPress={this.reset}>
+                     <View style={styles.fullScreen}>
+                     <Text style={styles.YourScoreText}>Your Score:</Text>
+                     <Text style={styles.scoreGameOver}>{this.state.score}</Text>
+                         <Text style={styles.gameOverText}>Game Over</Text>
+                         <Text style={styles.gameOverSubText}>Try Again</Text>
+                     </View>
+                 </TouchableOpacity>}
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+      flex: 1,
+      backgroundColor: '#fff',
   },
-  engine: {
+  backgroundImage: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      width: Constants.MAX_WIDTH,
+      height: Constants.MAX_HEIGHT
+  },
+  gameContainer: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+  },
+  gameOverText: {
+      color: 'white',
+      fontSize: 48,
+      fontFamily: '04b_19'
+  },
+  YourScoreText: {
     position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    color: 'yellow',
+    fontSize: 48,
+    top: 48,
 
-export default App;
+    textShadowColor: '#444444',
+    textShadowOffset: { width: 2, height: 2},
+    textShadowRadius: 2,
+    fontFamily: '04b_19'
+  },
+  gameOverSubText: {
+      color: 'white',
+      fontSize: 24,
+      fontFamily: '04b_19'
+  },
+  fullScreen: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'black',
+      opacity: 0.8,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  score: {
+      position: 'absolute',
+      color: 'white',
+      fontSize: 72,
+      top: 50,
+      left: Constants.MAX_WIDTH / 2 - 20,
+      textShadowColor: '#444444',
+      textShadowOffset: { width: 2, height: 2},
+      textShadowRadius: 2,
+      fontFamily: '04b_19'
+  },
+
+  scoreGameOver: {
+      position: 'absolute',
+      color: 'white',
+      fontSize: 72,
+      top: 100,
+      left: Constants.MAX_WIDTH / 2 - 20,
+      textShadowColor: '#444444',
+      textShadowOffset: { width: 2, height: 2},
+      textShadowRadius: 2,
+      fontFamily: '04b_19'
+  },
+  fullScreenButton: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      flex: 1
+  }
+});
